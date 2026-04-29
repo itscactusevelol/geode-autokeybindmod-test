29c86202f3bf6d26d32294ee68c3025db47f524d
itscactusevelol <event2@yahoo.com>
2026-04-29 17:08:02 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,69 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <windows.h>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+bool isTriggered = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+// Helper function to simulate pressing and releasing a key
+void simulateKeypress(WORD vKey) {
+    INPUT inputs[2] = {};
+    ZeroMemory(inputs, sizeof(inputs));
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    // Press down
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = vKey;
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    // Release
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = vKey;
+    inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+}
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+class $modify(PlayLayer) {
+    void postUpdate(float p0) {
+        PlayLayer::postUpdate(p0);
+        
+        if (!Mod::get()->getSettingValue<bool>("enable-mod")) return;
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        // Prevent division by zero at the very start of the level
+        if (this->m_levelLength == 0) return;
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        // Calculate percentage
+        int percent = (this->m_player1->getPositionX() / this->m_levelLength) * 100;
+        int triggerPercent = Mod::get()->getSettingValue<int>("percentage");
+
+        if (percent >= triggerPercent && !isTriggered) {
+            simulateKeypress(VK_F9); // Simulates F9
+            isTriggered = true;
+            Notification::create("Auto-Keybind Triggered!", NotificationIcon::Success)->show();
+        }
+    }
+
+    void destroyPlayer(PlayerObject* player, GameObject* object) {
+        PlayLayer::destroyPlayer(player, object);
+        if (isTriggered) {
+            simulateKeypress(VK_F9); 
+            isTriggered = false;
+        }
+    }
+
+    void levelComplete() {
+        PlayLayer::levelComplete();
+        if (isTriggered) {
+            simulateKeypress(VK_F9); 
+            isTriggered = false;
+        }
+    }
+
+    void onQuit() {
+        if (isTriggered) {
+            simulateKeypress(VK_F9); 
+            isTriggered = false;
+        }
+        PlayLayer::onQuit();
+    }
 };
